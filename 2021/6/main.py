f396a9edd70e981598c34a5bffb817347ecb11e5
Wim <wbongers@gmail.com>
2021-12-06 20:27:25 +0100

    No maths but it works

---
+++ 2021/6/main.py
@@ -0,0 +1,45 @@
+import re
+
+def part_one(file):
+    count = runDays(file,80)
+    print('P1 ' + file + ': ' + str(count))
+    
+def part_two(file):
+    count = runDays(file,256)
+    print('P2 ' + file + ': ' + str(count))
+    
+
+def runDays(file,days):
+    
+    lines = read_file(file)
+    fishCount = [0]*9
+
+    for age in lines:
+        fishCount[age] += 1
+    
+    day = 0
+    
+    while day < days:
+        ageZero = fishCount[0]
+        j = 0
+        while j < 8:
+            fishCount[j] = fishCount[j+1] 
+            j += 1
+        fishCount[6] += ageZero
+        fishCount[8] = ageZero
+    
+        day += 1
+    return sum(fishCount)
+
+      
+def read_file(file):
+    text_file = open("./input/" + file, "r")
+    file_contents = text_file.read()
+    lines = file_contents.splitlines()[0].split(',')
+
+    return list(map(int, lines))
+    
+part_one('test.txt')
+part_one('real.txt')
+part_two('test.txt')
+part_two('real.txt')
