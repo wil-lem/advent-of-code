45c54f67cda00086c44d2a14b84d3577f078c9b6
Wim <wbongers@gmail.com>
2024-12-09 22:56:09 +0100

    Still struggling

--- 2024/6/main.py
+++ 2024/6/main.py
@@ -1,119 +1,253 @@
 import re
 from readFile import ReadFile
-
-def parseFile(filename):
-    rf = ReadFile(filename)
-    lines = rf.getLines()
+import time
     
-
 class Vector:
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
-    
-    def set(self,x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+    
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+    
     def clone(self):
         return Vector(self.x, self.y)
 
-    def move(self,axis,mod):
+    def set(self, axis, value):
         if axis == 'x':
-            self.x += mod
+            self.x = value
         else:
-            self.y += mod
+            self.y = value
+        pass
+
+    def add(self, other):
+        self.x += other.x
+        self.y += other.y
+        pass
+
+    def inGrid(self,w,h):
+        return self.x >= 0 and self.x < w and self.y >= 0 and self.y < h
     
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __str__(self):
-        return '(' + str(self.x) + ', '+ str(self.y) + ')'
+class MoveMent:
+    def __init__(self, axis, direction):
+        self.mod = Vector(0,0)
+        self.mod.set(axis, direction)
         pass
+    
+    def apply(self, point):
+        point.add(self.mod)
 
-class Line:
-    def __init__(self,x,y):
-        self.start = Vector(x,y)
-        pass
+    def clone(self):
+        movement = MoveMent(self.mod.x, self.mod.y)
+        movement.mod = self.mod.clone()
+        return movement
 
-class Position:
-    def __init__(self,x,y,char):
-        self.v = Vector(x,y)
-        self.char = char
-        self.visited = False
-
-    def setVisited(self):
-        self.visited = True
-
-    def isOpen(self):
-        return self.char != '#'
-
-
-
-    def __str__(self):
-        return 'Position: ' + str(self.v) + ' C:' + self.char
-        pass
-
-class Guard:
-    def __init__(self,x,y):
-        self.position = Vector(x,y)
-        self.axis = 'y'
-        self.direction = -1
-        self.map = []
-        pass
-
-    def nextPosition(self):
-        nextPosition = self.position.clone()
-        nextPosition.move(self.axis,self.direction)
-
-        matches = [ pos for pos in self.map if pos.v == nextPosition]
-        
-        if len(matches) == 0:
-            return False
-        
-        return matches[0]
-
-    def turn(self):
-        if self.axis == 'y': 
-            self.direction *= -1
-            self.axis = 'x'
+    def turnClockWise(self):
+        if self.mod.x == 0:
+            if self.mod.y == 1:
+                # Down -> Left
+                newMod = Vector(-1, 0)
+            else:
+                # Up -> Right
+                newMod = Vector(1, 0)
         else:
-            self.axis = 'y'
-        
-        
-    def move(self):
-        next = self.nextPosition()
+            if self.mod.x == 1:
+                # Right -> Down
+                newMod = Vector(0, 1)
+            else:
+                # Left -> Up
+                newMod = Vector(0, -1)
 
-        if next == False:
+        self.mod = newMod
+
+class Map:
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+        self.obstructions = {}
+        self.guard = None
+        self.inSimulation = False
+
+        self.extraObstructions = []
+        
+        pass
+
+    def addGuard(self, guard):
+        self.guard = guard
+
+    def addObstruction(self, x, y):
+        self.obstructions[(x,y)] = True
+    
+    def removeObstruction(self, x, y):
+        del self.obstructions[(x, y)]
+
+    def isObstructed(self, x, y):
+        return self.obstructions.get((x, y), False)
+
+    def doRoute(self):
+        steps = 0
+        while self.moveGuard():
+            steps += 1
+        pass
+        
+    def moveGuard(self):
+        nextPos = self.guard.nextPos()
+        if not nextPos.inGrid(self.w, self.h):
             return False
-        if not next.isOpen():
-            self.turn()
-            # Assume we'll always end up exiting the screen.
+        if self.isObstructed(nextPos.x, nextPos.y):
+            self.guard.turn()
+            if self.inSimulation and self.guard.inLoop:
+                return False
             return True
-        self.position = next.v.clone()
-        next.setVisited()
-        return True
+        else:
+            # We can move to the next position, before we move,
+            # put an obstruction in the next position and start the simulation
+            if not self.inSimulation:
+                self.simulateObstacle(nextPos)
+                
+
+            self.guard.moveToNext(nextPos)
+            
+            if self.inSimulation and self.guard.inLoop:
+                return False
+            return True
+        
+    def simulateObstacle(self,nextPos):
+        self.inSimulation = True
+        self.guard.saveState()
+        self.addObstruction(nextPos.x, nextPos.y)
+
+        self.doRoute()
+        if self.guard.inLoop and nextPos not in self.extraObstructions:
+            self.extraObstructions.append(nextPos.clone())
+        
+        self.removeObstruction(nextPos.x, nextPos.y)
+        self.guard.restoreState()
+        self.inSimulation = False
+
+        # Add extra obstruction
+        # self.detectLoop = True
+
+        # self.detectLoop = False
+
+    def printMap(self):
+        for y in range(self.h):
+            for x in range(self.w):
+                if self.isObstructed(x, y):
+                    print('#', end='')
+                elif Vector(x, y) in self.extraObstructions:
+                    print('O', end='')
+                elif self.guard.pointsOnRoute.get((x, y), False):
+                    print('X', end='')
+                else:
+                    print('.', end='')
+            print()
+        pass
+
+        
+class Guard:
+    def __init__(self, x, y, axis, direction):
+        self.position = Vector(x, y)
+        self.movement = MoveMent(axis, direction)
+        self.pointsOnRoute = {}
+        self.states = {}
+        self.inLoop = False
+        pass
+
+    def nextPos(self):
+        pos = self.position.clone()
+        self.movement.apply(pos)
+        return pos
     
+    def turn(self):
+        self.movement.turnClockWise()
+
+        key = self.getStateKey()
+        if self.hasState(key):
+            self.inLoop = True
+            return
+        self.addState(key)
+
+
+    def addPointOnRoute(self, point):
+        self.pointsOnRoute[(point.x, point.y)] = True
+
+    def addState(self, state):
+        self.states[state] = True
+
+    def hasState(self, state):
+        return self.states.get(state, False)
+
+    def getStateKey(self):
+        return f"{self.position.x},{self.position.y},{self.movement.mod.x},{self.movement.mod.y}"
+
+    def moveToNext(self, nextPos):
+        # nextPos = self.nextPos()
+        self.addPointOnRoute(nextPos)
+        self.position = nextPos
+
+        key = self.getStateKey()
+        if self.hasState(key):
+            self.inLoop = True
+            return
+        self.addState(key)
+
+
+    def saveState(self):
+        self.savedStates = self.states.copy()
+        self.savedPosition = self.position.clone()
+        self.savedMovement = self.movement.clone()
+        self.savedPointsOnRoute = self.pointsOnRoute.copy()
+    
+    def restoreState(self):
+        self.states = self.savedStates
+        self.position = self.savedPosition
+        self.movement = self.savedMovement
+        self.pointsOnRoute = self.savedPointsOnRoute
+        self.inLoop = False
+
+
 def one(fileName):
+    # Track the time it takes to solve the problem
+    start = time.time()
+
     rf = ReadFile('input/' + fileName + '.txt')
     lines = rf.getLines()
-    grid = []
-    guard = Guard(0,0)
-    for y in range(len(lines)):
-        line = lines[y]
-        for x in range(len(line) - 1):
-            pos= Position(x,y,line[x])
-            if pos.char == '^':
-                pos.char = '.'
-                pos.setVisited()
-                guard.position = pos.v.clone()
-            guard.map.append(pos)
+    w = h = 0
+    h = len(lines)
+    w = len(lines[0].strip())
+    startPos = Vector(0, 0)
 
-    move = 0
-    while guard.move():
-        move += 1
-
-    print('Total:', len([pos for pos in guard.map if pos.visited]))
+    map = Map(w, h)
     
+    for y in range(h):
+        for x in range(w):
+            char = lines[y][x]
+            if char == '#':
+                map.addObstruction(x, y)
+            elif char == '^':
+                guard = Guard(x, y, 'y', -1)
+                map.addGuard(guard)
+                startPos = Vector(x, y)
+
+    map.doRoute()
+    print('Points visited:', len(map.guard.pointsOnRoute))
+    print('Extra obstructions:', len(map.extraObstructions))
+    print('Start poistion in obstruction:', startPos in map.extraObstructions)
+    
+    outsideGrid = [obs for obs in map.extraObstructions if not obs.inGrid(w, h)]
+    doubeObstructions = [obs for obs in map.extraObstructions if map.isObstructed(obs.x, obs.y)]
+    print('Outside grid:', len(outsideGrid))
+    print('Bouble obs:', len(doubeObstructions))
+    
+
+    # map.printMap()
+    end = time.time()
+    print(f"Time: {end - start}")
+
 one('test')
 one('real')
+
